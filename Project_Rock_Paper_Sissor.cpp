b68cecef5d89d2dd26de19c1bf015894bf0b3e10
BOT47 <pubg58375@gmail.com>
2025-06-30 17:18:32 +0500

    Rock Paper Scissor V1

---
+++ Project_Rock_Paper_Sissor.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+#include <string.h>
+#include <math.h>
+#include <map>
+using namespace std;
+
+/*----Game is based on Computer vs Player----*/
+void R_P_S() 
+ {
+     cout << "---------------------------\n" ;
+     cout <<              "|"               ;
+     cout <<              "|"               ;
+     cout <<              "|"               ;
+     cout << "---------------------------\n" ;
+ }
+
+int main()
+{ 
+    // R , P , S
+    map <int , string> RPS= {{1 , " ____\n|    |\n|____/\n|    \\\n|     \\\n"} , {2 , " ____\n|    |\n|____/\n|    \n|    \n"}, {3 , " ______\n|\n|______\n       |\n ______|\n"} } ;
+    
+
+    //-----------------//
+    cout << "\n" ;
+    return 0;
+ }
---
+++ test_code.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+#include <string.h>
+#include <math.h>
+using namespace std;
+void func()
+{
+}
+int main()
+{
+  /*cout  << " ____\n" ;
+    cout  << "|    |\n" ; 
+    cout  << "|____/\n" ;
+    cout  << "|    \\\n" ;
+    cout  << "|     \\\n" ;
+    cout << "\n" ; */
+//   cout <<  " ____\n" << "|    |\n" << "|____/\n" << "|    \\\n" << "|     \\\n" ;
+//   cout << "\n" ;
+//   cout <<  " ____\n|    |\n|____/\n|    \\\n|     \\\n" ;
+//   cout << "\n" ;
+  //--R successfully made above--//
+  // Down is P
+    // cout  << " ____\n" ;
+    // cout  << "|    |\n" ; 
+    // cout  << "|____/\n" ;
+    // cout  << "|    \n" ;
+    // cout  << "|    \n" ;
+
+    // cout  << " ____\n|    |\n|____/\n|    \n|    \n" ;
+    // NOW DOWN IS S ;
+    // cout << " ______\n" ;
+    // cout << "|\n" ;
+    // cout << "|______\n" ;
+    // cout << "       |\n" ;
+    // cout << "_______|\n" ;
+  cout << " ______\n|\n|______\n       |\n ______|\n" ;
+  return 0;
+}
